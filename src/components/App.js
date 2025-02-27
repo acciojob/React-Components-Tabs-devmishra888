98cc593c077919ca0828dc57c1fefcf893450a5b
Dev Mishra <devmishra888@gmail.com>
2025-02-27 21:26:35 +0530

    tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
+import React, {useState} from "react";
 
+const App=()=>{
+    const [tap, setTap] = useState("")
+
+
+
+    return(
+        <div>
+            <div className=" tabs">
+            <button className=" tab" id="Home-tab" onClick={()=>setTap("Home")}>Home</button>
+            <button  className=" tab" id="About-tab" onClick={()=>setTap("About")}>About</button>
+            <button className="tab" id="Features-tab" onClick={()=>setTap("Features")}>Features</button>
+            </div>
+
+            <div className="viewport">
+         {tap == "Home" && <p>Home, Pages Go Here .</p>}
+         {tap == "About" && <p>About, Pages Go Here .</p>}
+         {tap == "Features" && <p>Features, Pages Go Here .</p>}
+            </div>
+        </div>
+    )
+}
+
+export default App;
